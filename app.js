ee934f5def88a0b400f50e07f8fea857a52e0e99
Adrien <adrien@aggery.com>
2015-05-22 22:28:02 +0200

    Handle drone discovery and connection

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 var noble = require('noble');
 
-var devices = [];
+var RS_uuid = '078b9f9b666a482d87472ba38d7e5e26';
 
-noble.state = 'poweredOn';
+noble.on('stateChange', function(state) {
+	if (state === 'poweredOn') {
+		noble.startScanning();
+		console.log("Started scanning");
+	} else {
+		noble.stopScanning();
+		console.log("Stopped scanning");
+	}
+});
 
-noble.startScanning(devices, true);
+noble.on('discover', function(device){
+	if (device.uuid == RS_uuid)
+	{
+		console.log("RS drone found.");
+		noble.stopScanning();
+		RS_connect(device);
+	}
+});
 
-setInterval(function () {
-	console.log("--- Showing devices ---");
-	console.log(devices);
-}, 1000);
+function RS_connect(drone)
+{
+	drone.on('disconnect', function(){
+		console.log("Lost connection to RS drone.");
+		process.exit(0);
+	});
+	drone.connect(function(error)
+	{
+		if (error){
+			console.log("Error while connecting to the drone: " + error);
+			process.exit(0);
+		}
+		else {
+			console.log("Connected to RS drone.");
+		}
+	});
+}
